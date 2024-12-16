1d1e46587acb29d63a407b7382032fab802735d7
Guilherme615 <118912756+Guilherme615@users.noreply.github.com>
2024-12-16 16:35:40 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const app = require('express')();
+
+app.listen(3000, () => console.log('Server running'));
+
+app.get('/skills', (req, res) => {
+    res.send({
+        javascript: 10,
+        node: 10
+    })
+})
